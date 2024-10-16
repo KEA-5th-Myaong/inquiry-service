3559a4fac7d96e3416d585df871700c4f981148b
Sujin Kang <1214sujin@naver.com>
2024-10-16 20:07:15 +0900

    :sparkles: feature: 데이터 초기화 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,93 @@
+insert into "inquiry_author" (member_id, username, created_at, updated_at) values
+	(1, 'super', now(), now()),
+	(2, 'admin1', now(), now()),
+	(3, 'admin2', now(), now()),
+	(4, 'admin3', now(), now()),
+	(5, 'member1', now(), now()),
+	(6, 'member2', now(), now()),
+	(7, 'member3', now(), now()),
+	(8, 'member4', now(), now()),
+	(9, 'member5', now(), now());
+
+insert into "inquiry" (inquiry_id, member_id, title, content, is_secret, created_at, updated_at) values
+	(1, 8, 'Ullam quisquam ullam ea.', 'Nesciunt ipsum id voluptate natus at culpa. At eum ad accusamus fuga non. Illo doloribus ducimus fuga.
+Incidunt ab explicabo. Minus iusto quod ex facere perferendis. Deleniti vitae placeat occaecati.', false, now(), now()),
+	(2, 8, 'Tempora corrupti labore.', 'Neque quibusdam quaerat libero. Dolores reiciendis voluptas. Labore ipsam reiciendis assumenda quae.', false, now(), now()),
+	(3, 6, 'Corporis nostrum accusantium.', 'Possimus id fuga nemo velit sunt alias. Vel fugit natus maiores.
+Vel placeat in cumque.
+Delectus alias aliquid praesentium. Voluptatum explicabo error nobis esse repellat itaque saepe.', false, now(), now()),
+	(4, 6, 'Corrupti sint dignissimos.', 'Voluptates a officia blanditiis officiis nemo nulla magnam.
+Reprehenderit nam fugit mollitia natus qui error. Voluptate dolorum porro tempora.', true, now(), now()),
+	(5, 6, 'Pariatur molestias pariatur ipsa.', 'Sint fugiat ullam dolor mollitia nobis. Neque est itaque. Corrupti maxime voluptatum ullam nobis ipsum dicta.
+Minima at omnis asperiores animi cumque.', false, now(), now()),
+	(6, 9, 'Animi et modi.', 'Tempora aspernatur illum consectetur nam neque delectus. Culpa modi qui doloremque debitis. Sunt pariatur praesentium corrupti fugit excepturi.', true, now(), now()),
+	(7, 9, 'Adipisci occaecati similique architecto harum.', 'Repudiandae aspernatur porro nostrum. Eum maiores quae beatae quas voluptatem neque. Sapiente ipsam corporis libero nulla accusamus error.', false, now(), now()),
+	(8, 7, 'Sapiente deserunt porro.', 'Odio aperiam sint ex aperiam.
+Aperiam eos sequi fuga nostrum ipsum. Distinctio minima omnis.', false, now(), now()),
+	(9, 6, 'Officia sint vitae ut. Sint magnam illum minima.', 'Commodi corporis nisi illum distinctio harum excepturi temporibus. Error sint dolorem laboriosam molestiae.
+Minus est occaecati quod voluptas ab. Recusandae ab ipsam reiciendis commodi dolores.', true, now(), now()),
+	(10, 8, 'Ducimus veritatis corporis similique.', 'Tempora excepturi molestias cum perspiciatis. Nemo eius minus earum. Libero sed accusantium occaecati ea.', false, now(), now()),
+	(11, 7, 'Iure doloribus recusandae facere quibusdam velit.', 'Illo expedita est explicabo. Placeat molestias nemo officia. Ad necessitatibus omnis mollitia.
+Laudantium tempore sed impedit fugit. Officia soluta eos dolore nemo.', true, now(), now()),
+	(12, 9, 'Vel eos debitis.', 'Accusantium voluptatem optio labore error. Voluptatem voluptates neque occaecati odit sint tempore repellat.', false, now(), now()),
+	(13, 7, 'Corporis vero similique qui.', 'Praesentium accusantium pariatur nam. Quaerat aspernatur laborum commodi adipisci. Excepturi sapiente asperiores iusto facilis quis praesentium.', false, now(), now()),
+	(14, 9, 'Iusto voluptates totam.', 'Iure cum nisi alias magni expedita. Magnam voluptatem architecto molestiae provident.
+Laudantium dolorem sit repellat quo ipsam. Inventore odit porro rem. Quidem vel dolor itaque ipsa ab.', true, now(), now()),
+	(15, 8, 'Iusto laboriosam aliquam.', 'Vel quia temporibus voluptas. Temporibus culpa quibusdam sapiente deleniti porro. Ab quam error fuga nam.', false, now(), now()),
+	(16, 5, 'Officiis laborum vel asperiores.', 'Quae delectus maxime quia necessitatibus nobis. Totam sequi sapiente distinctio delectus officiis.
+Reiciendis dolorem repellendus illum quidem. Earum dolores eligendi quo laudantium mollitia.', true, now(), now()),
+	(17, 7, 'Quas ducimus eos architecto iure magni.', 'Unde ratione id tempora labore explicabo corrupti.
+Quibusdam officiis facere quam. Labore porro temporibus nam nihil. Aliquam ex illo placeat quasi.', true, now(), now()),
+	(18, 7, 'Excepturi molestiae dicta numquam doloremque quo.', 'Earum dolor quam necessitatibus accusantium omnis nesciunt. Neque totam nobis quos.', false, now(), now()),
+	(19, 5, 'Quaerat tempore est quo repellendus nemo totam.', 'Commodi nobis debitis incidunt optio ullam iusto in. Magnam deleniti accusamus praesentium incidunt itaque dolorem. Optio corporis vel magnam veniam.', true, now(), now()),
+	(20, 7, 'Quasi distinctio corporis officia.', 'Tenetur officiis optio quam harum sunt. Doloremque assumenda quam cumque quod debitis. Sunt est architecto quasi. Esse magni soluta cumque est fugiat numquam.
+Praesentium magnam ipsam labore.', true, now(), now()),
+	(21, 5, 'Eos delectus in voluptas ad aut.', 'Autem aspernatur quisquam. Recusandae omnis voluptatem autem cum ea. Excepturi quis labore aliquid rem.', false, now(), now()),
+	(22, 5, 'Nam a quas explicabo vitae accusantium adipisci.', 'Repellat quasi veritatis magni natus ipsum ab. Debitis maxime aliquam magni eum explicabo minus enim. Excepturi perspiciatis quaerat veniam fuga.', false, now(), now()),
+	(23, 9, 'Pariatur vero doloribus a. Earum sit quasi.', 'Molestias mollitia corporis ipsa consequuntur molestiae. Molestias iusto maiores totam magnam sed.
+Laborum doloremque inventore cupiditate error voluptas aliquam alias. Placeat vero fugiat.', true, now(), now()),
+	(24, 8, 'Quo dolores nihil quae quis nam minus.', 'Incidunt ad itaque delectus sit excepturi aliquid. Expedita quos odio.
+Occaecati velit ut culpa at illo officia necessitatibus. Et nihil ipsum quidem quasi libero nostrum.', false, now(), now()),
+	(25, 8, 'Placeat est suscipit.', 'Dolorum tempora eaque ipsa aliquam impedit. Vel earum in quaerat fuga.
+Eligendi nam eligendi culpa fuga fuga molestiae. Dolorem iusto voluptas deleniti. Minima nostrum accusantium quo odio.', false, now(), now()),
+	(26, 7, 'Tempora sed earum explicabo magnam.', 'Enim repellat illo nihil ipsa nam illo. Sint quo voluptatem dignissimos laboriosam saepe reprehenderit. Est nemo quaerat occaecati nobis aperiam eum.', true, now(), now()),
+	(27, 8, 'Est reiciendis voluptate impedit.', 'Voluptatum quia tempora hic alias voluptas. Quod voluptas accusamus vero iure laudantium quos. Soluta maiores est voluptas.
+Suscipit quam consectetur.', false, now(), now()),
+	(28, 9, 'Fugit non porro id distinctio doloremque.', 'Id dolorum debitis voluptate eligendi deleniti. Ad hic explicabo impedit cum.
+Iste rerum aliquid voluptate possimus. Ab consequuntur voluptate est minus commodi.', false, now(), now()),
+	(29, 5, 'Minus maiores esse nisi.', 'Tempora corporis aliquid odit. Illo deserunt architecto quas pariatur reiciendis.', false, now(), now()),
+	(30, 8, 'Perspiciatis nulla ipsa aperiam minus animi ut.', 'Aut deserunt eveniet labore aliquam non. Aperiam ab autem veniam assumenda excepturi suscipit. Qui pariatur praesentium pariatur quia. Veniam sunt maxime illo.', true, now(), now());
+
+insert into "inquiry_reply" (inquiry_reply_id, inquiry_id, member_id, content, created_at, updated_at) values
+	(1, 1, 3, 'Excepturi laudantium est ea cumque totam quidem odio. Ipsa praesentium pariatur impedit.', now(), now()),
+	(2, 2, 1, 'Nostrum recusandae nesciunt harum. Iusto quaerat quibusdam vitae ut officiis laboriosam possimus.
+Alias unde minus pariatur. Ipsa voluptatem incidunt quae.', now(), now()),
+	(3, 3, 2, 'Asperiores laborum labore dolores quo vel. Ducimus possimus officia officia ea. Fuga eum odio laborum.
+Tempora magni omnis commodi accusantium eius esse. Quia recusandae tempora sint.', now(), now()),
+	(4, 4, 2, 'Cumque beatae fugit. Accusantium mollitia sint sequi vero aut.
+Quis culpa adipisci saepe. Voluptate nesciunt sit occaecati doloribus.', now(), now()),
+	(5, 5, 4, 'Molestias fugit necessitatibus. Cumque hic soluta harum. Fuga qui quos voluptatibus totam assumenda quidem.
+At autem voluptatem. Assumenda non sed nobis. Iure esse impedit quod.', now(), now()),
+	(6, 6, 2, 'Nobis velit molestiae at asperiores repudiandae. Atque autem minima recusandae facere blanditiis. Nemo hic similique aspernatur quod.', now(), now()),
+	(7, 7, 2, 'Ipsam commodi facilis quisquam quam quo. Officia corrupti et non. Porro culpa labore vel nobis.
+Amet perferendis rem accusamus quisquam quod enim. Laborum illum sed perspiciatis velit.', now(), now()),
+	(8, 8, 4, 'Sed voluptatibus eos dolores ex. Officia in temporibus odio repellendus.
+Quibusdam quibusdam iusto architecto. Minima voluptatum ex veniam aliquid eligendi optio.', now(), now()),
+	(9, 9, 1, 'Eaque enim amet eaque optio possimus quidem. Tempore quibusdam alias atque quidem unde.
+Recusandae quaerat doloremque aliquam harum suscipit esse. Cum reiciendis eum illo soluta ducimus.', now(), now()),
+	(10, 10, 2, 'Aliquam fuga ut repellat eveniet. Vero cumque eius soluta necessitatibus. Sequi aliquam consectetur error quam itaque.', now(), now()),
+	(11, 11, 4, 'Quisquam pariatur nostrum reiciendis blanditiis ipsam. Illo harum rerum iusto aliquam nobis. Eveniet voluptatum nobis commodi iure eius expedita.', now(), now()),
+	(12, 12, 4, 'Quia maxime tempora officiis aliquid sapiente.
+Ut odit sequi rem non. Officiis in delectus itaque nobis molestiae iure ea.', now(), now()),
+	(13, 13, 4, 'Rerum consequuntur accusantium maiores. Rerum rerum adipisci cum.', now(), now()),
+	(14, 14, 3, 'Odit ex voluptates quibusdam harum. Ducimus nisi itaque veritatis impedit nulla culpa.
+Ea praesentium provident. Officiis minus vitae cumque hic quos optio.', now(), now()),
+	(15, 15, 1, 'Quas voluptatem vel natus voluptate cupiditate veritatis. Maxime delectus est accusantium. Ipsam alias assumenda.
+Doloremque iusto quasi. Voluptas eum reiciendis tenetur.', now(), now()),
+	(16, 16, 2, 'Eum explicabo quisquam eligendi velit amet mollitia. Impedit quo placeat corrupti esse voluptate consectetur. At repellendus occaecati voluptatum a. Ex quia alias nobis.', now(), now()),
+	(17, 17, 1, 'Odio ipsam nam qui consequuntur minus. Exercitationem eligendi quam voluptas alias doloribus nam. Laborum reprehenderit voluptatum explicabo.
+Aliquam nostrum ipsa quidem voluptatum magnam.', now(), now()),
+	(18, 18, 4, 'Adipisci harum numquam autem vitae mollitia. Inventore nobis sunt dolore optio cumque similique.', now(), now()),
+	(19, 19, 2, 'Nulla debitis quo repellendus optio recusandae cum quas. At quasi maxime illum error laborum. Saepe at accusamus quaerat itaque.', now(), now()),
+	(20, 20, 3, 'Placeat sed corrupti enim nostrum beatae doloremque. Quaerat sapiente facilis quia sapiente magni. Incidunt pariatur odit atque voluptates.', now(), now());
+
